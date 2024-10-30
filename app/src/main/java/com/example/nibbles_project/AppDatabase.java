472d050ee36205e204f8b3402c81d597394a876f
MokshaDK <mokshakothari05@gmail.com>
2024-10-30 12:31:14 +0530

    commit

--- app/src/main/java/com/example/nibbles_project/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.nibbles_project;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Intake.class}, version = 1, exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract IntakeDao intakeDao();
-}
