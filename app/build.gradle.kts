fe0efc128defca026189e95c93b3334b33951013
MokshaDK <mokshakothari05@gmail.com>
2024-10-30 18:16:35 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,15 +37,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation(libs.activity) // Use KTX version for Firestore
+    implementation(libs.activity)
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-firestore-ktx") // Use KTX version for Firestore
+    implementation("com.google.firebase:firebase-firestore-ktx")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.recyclerview:recyclerview:1.3.0") // Update with correct version
     implementation("com.github.bumptech.glide:glide:4.15.0") // Glide dependency
-    //kapt("com.github.bumptech.glide:compiler:4.15.0") // Glide compiler for annotations
     implementation("com.google.android.gms:play-services-location:19.0.1") // Google Play services
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
